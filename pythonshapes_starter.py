b811528406d955048bfd74a2449591fdc0c427cd
jpotts8 <jp.z.potts@gmail.com>
2017-07-06 14:56:27 -0500

    Add files via upload

---
+++ pythonshapes_starter.py
@@ -0,0 +1,23 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(1000,1000)
+x_pos = 0
+y_pos = 0
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+pen down
+color- input"Favorite Color"
+input= number of sides
+change i for user input
+input 4 sides
+
+
+
+# Close window on click.
+exitonclick()
